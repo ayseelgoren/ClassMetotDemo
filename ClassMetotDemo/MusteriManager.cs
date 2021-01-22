79c62fe835d2e39aa3bafb205ca06535c4d7b1d2
Ayşe Elgören <ayseelgoren@gmail.com>
2021-01-22 16:14:21 +0300

    commit

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+
+
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine(musteri.ad + " " + musteri.soyad + " isimli kullanıcı eklenmiştir.");
+        }
+        public void Sil(Musteri musteri)
+        {
+            Console.WriteLine(musteri.ad + " " + musteri.soyad + " isimli kullanıcı silinmiştir.");
+        }
+        public void Listele(Musteri musteri)
+        {
+            Console.WriteLine("İd :"+musteri.id);
+            Console.WriteLine("Ad :"+musteri.ad);
+            Console.WriteLine("Soyad :"+musteri.soyad);
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            MusteriManager manager = new MusteriManager();
+           
+            Musteri m1 = new Musteri();
+            m1.id = 1;
+            m1.ad = "Ali";
+            m1.soyad = "Kemal"; 
+            manager.Listele(m1);
+            manager.Ekle(m1);
+            manager.Sil(m1);
+           
+
+            Musteri m2 = new Musteri();
+            m2.id = 2;
+            m2.ad = "Can";
+            m2.soyad = "Akar";
+            manager.Listele(m2);
+            manager.Ekle(m2);
+            manager.Sil(m2);
+    
+
+            Musteri m3 = new Musteri();
+            m3.id = 3;
+            m3.ad = "Yahya";
+            m3.soyad = "Demir";
+            manager.Listele(m3);
+            manager.Ekle(m3);
+            manager.Sil(m3);
+     
+
+
+        }
+    }
+}
